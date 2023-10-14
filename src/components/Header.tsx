9e33b43cf866eb1739d0cd232782e62672fdacdb
EhsanKey <ehsanKeyhani82@gmail.com>
2023-10-14 11:39:38 +0330

    Added Link component to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <div className="flex bg-purple-600 px-2 py-2 text-white">
       <nav className="text-sm font-bold hover:underline cursor-pointer md:text-lg lg:text-xl">
-        Next URL Shortener
+        <Link href="/">Next URL Shortener</Link>
       </nav>
     </div>
   );
